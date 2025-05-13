107b54c34a52032ca756748ada616293e51e74ff
haerangssa <khj100494@naver.com>
2025-05-13 23:05:28 +0900

    Fix: app name in rest_framework setting

--- houscan/settings.py
+++ houscan/settings.py
@@ -89,7 +89,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
-    'EXCEPTION_HANDLER': 'config.utils.custom_exception_handler',
+    'EXCEPTION_HANDLER': 'houscan.utils.custom_exception_handler',
 }
 
 SITE_ID = 1
