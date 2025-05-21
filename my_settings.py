04b099840e6395aeb9df75552e129e0eaaa8b708
haerangssa <khj100494@naver.com>
2025-05-21 17:19:12 +0900

    Fix: DB_HOST

--- my_settings.py
+++ my_settings.py
@@ -6,7 +6,7 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME', 'HOUSCAN'),               
         'USER': os.getenv('DB_USER', 'root'),                   
         'PASSWORD': os.getenv('DB_PASSWORD',''),    
-        'HOST': os.getenv('DB_HOST', 'localhost'),        
+        'HOST': os.getenv('DB_HOST', '127.0.0.1'),        
         'PORT': os.getenv('DB_PORT', '3306'),    
         'OPTIONS':  {'charset': 'utf8mb4'},
     }
