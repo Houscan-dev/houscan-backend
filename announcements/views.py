a9bb9e1f1f5590cdfc5314830eae80271f3618b3
haerangssa <khj100494@naver.com>
2025-05-21 12:52:54 +0900

    Fix: announcements list

--- announcements/views.py
+++ announcements/views.py
@@ -1,15 +1,15 @@
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
 from .models import Announcement
-from .serializers import AnnListSerializer,AnnDetailSerializer
+from .serializers import AnnListSerializer #AnnDetailSerializer
 
 class AnnouncementListAPIView(generics.ListAPIView):
     permission_classes = [AllowAny]  
     queryset = Announcement.objects.order_by('-updated_at')
     serializer_class = AnnListSerializer
 
-class AnnouncementDetailAPIView(generics.ListAPIView):
+'''class AnnouncementDetailAPIView(generics.ListAPIView):
     permission_classes = [AllowAny]
     lookup_field = 'file_name'
     queryset = Announcement.objects.all()
-    serializer_class = AnnDetailSerializer
+    serializer_class = AnnDetailSerializer'''
